e5ea01cd83f7389276925c45c535d314268a47c2
Miiaroma <miia.romanainen@gmail.com>
2019-01-08 15:07:39 +0200

    Cat Program done.

---
+++ OOHarjoitukset/kissaohjelma/Cat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kissaohjelma
+{
+    class Cat
+    {
+        public string name;
+        public int weight;
+
+        public Cat()
+        {
+            this.weight = 1;
+            //Console.WriteLine($"Kissan paino on alustettu arvoksi {this.weight}");
+        }
+
+        public Cat(string name, int weight)
+        {
+            this.name = name;
+            this.weight = weight;
+            Console.WriteLine($"Luotu kissa {this.name}, jonka paino on {this.weight}");
+        }
+
+        public Cat(int weight)
+        {
+            this.weight = weight;
+            //Console.WriteLine($"Kissan paino on {this.weight}");
+        }
+
+        public void IncreaseWeight(int value)
+        {
+            this.weight += value;
+
+            Console.WriteLine($"Kissan {this.name} uusi paino on {this.weight}");           
+        }
+
+        public void PrintWeight()
+        {
+            Console.WriteLine($"Kissan {this.name} paino on {this.weight}");
+        }
+    }
+}
---
+++ OOHarjoitukset/kissaohjelma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace kissaohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä on kissaohjelma");
+
+            Cat myFirstCat = new Cat("Pilli", 1);
+            myFirstCat.IncreaseWeight(1);
+            myFirstCat.PrintWeight();
+            // myFirstCat.weight = 5;
+            //Console.WriteLine($"Kissan paino on muutettu {myFirstCat.weight}");
+            Console.Write("-------------------------------------------------------\n");
+
+            Cat cat = new Cat("Pulla", 3);
+            cat.PrintWeight();
+            Console.Write("Press any key to continue...\n");
+            Console.ReadKey();
+        }
+    }
+}
