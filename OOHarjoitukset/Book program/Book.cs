4ed7cfd73009342f7677e21570f9d35dae416a7a
Miiaroma <miia.romanainen@gmail.com>
2019-01-09 10:08:55 +0200

    Book program initialized.

---
+++ OOHarjoitukset/Book program/Book.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book_program
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int idNumber;
+        public double price;
+
+        public Book()
+        {
+            this.price = 10.50;
+        }
+
+        //Constructor
+        public Book(string title, string author, int idNumber, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.idNumber = idNumber;
+            this.price = price;
+            Console.WriteLine($"Luotu kirja {this.title}, jonka kirjoittajan nimi on {this.author}, id-numero {this.idNumber}, hinta {this.price}");
+        }
+
+        public Book (double price)
+        {
+            this.price = price;
+            //Console.WriteLine($"Kirjan hinta on {this.price}");
+        }
+
+        public string CompareBooks(Book book)
+        {
+            string text = "";
+            Console.WriteLine($"Kirja: {this.title} - Hinta:{this.price.ToString()}\n");
+            Console.WriteLine($"Kirja: {book.title} - Hinta:{book.price.ToString()}\n");
+            if (this.price > book.price)
+            {
+                //Console.WriteLine($"Kirja: {this.title} Hinta:{this.price.ToString()} on kalliinpi\n");
+                text = $"Kirja: {this.title} Hinta: {this.price.ToString()} on kalliinpi\n";
+            }
+            else
+            {
+                //Console.WriteLine($"Kirja: {book.title} Hinta:{book.price.ToString()} on kalliinpi\n");
+                text = $"Kirja: {book.title} Hinta:{book.price.ToString()} on kalliinpi\n";
+            }
+            return text;        
+        }
+
+        public string PrintBookInfo()
+        {
+            //Console.WriteLine($"Kirjan tiedot ovat {this.title}, kirjoittaja {this.author}, id-numero {this.idNumber}, hinta {this.price}");
+            return $"Kirjan tiedot ovat {this.title}, kirjoittaja {this.author}, id-numero {this.idNumber}, hinta {this.price}";
+        }
+    }
+}
---
+++ OOHarjoitukset/Book program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Book_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä on kirjaohjelma");
+
+            Book newBook = new Book("Kirja1", "Kirjailija1", 12345, 10.50);
+            newBook.PrintBookInfo();
+            Console.Write("-------------------------------------------------------\n");
+
+            Book newBook2 = new Book("Kirja2", "Kirjailija2", 67890, 15.50);
+            //mySecondBook.PrintBookInfo();
+            Console.WriteLine(newBook2.PrintBookInfo());
+           
+
+            //newBook.CompareBooks(newBook2);
+            //Console.WriteLine(newBook.CompareBooks(newBook2);
+            //newBook2.CompareBooks(newBook);
+
+            string compareText = newBook.CompareBooks(newBook2);
+            Console.WriteLine(compareText);
+
+            Console.Write("Press any key to continue...\n");
+            Console.ReadKey();
+        }
+    }
+}
