639b1f4f765b009f563dc860686ac77b2a81c1b2
Miiaroma <miia.romanainen@gmail.com>
2019-01-17 17:06:57 +0200

    Employee program done.

---
+++ OOHarjoitukset/Employee program/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_program
+{
+    class Employee
+    {
+        public string name;
+        public string id;
+        public string position;
+        public double salary;
+
+        public Employee()
+        {
+            this.name = String.Empty; 
+            this.id = String.Empty; 
+            this.position = String.Empty;
+            this.salary = 0;
+        }
+
+        public Employee(string name, string id, string position, int salary)
+        {
+            this.name = name;
+            this.id = id;
+            this.position = position;
+            this.salary = salary;
+        }
+
+        public string CompareSalary(Employee employee)
+        {
+            if (this.salary > employee.salary)
+            {
+                return $"Työntekijän {this.name} palkkaa {this.salary} € on suurempi kuin työntekijän {employee.name} palkka {employee.salary} €.";
+            }
+
+            if (this.salary == employee.salary)
+            {
+                return $"Työntekijän {this.name} palkkaa {this.salary} € on yhtä suuri kuin työntekijän {employee.name} palkka {employee.salary} €.";
+
+            }
+            else
+            {
+                return $"Työntekijän {this.name} palkkaa {this.salary} € on pienempi kuin työntekijän {employee.name} palkka {employee.salary} €.";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Työntekijän nimi: {name}\ntunnus: {id}\ntehtävä: {position}\npalkka: {salary} €\n";
+        }
+    }
+}
---
+++ OOHarjoitukset/Employee program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Employee_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa työntekijöiden tiedot ja vertailee palkkoja\n");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("Matti", "001", "toimitusjohtaja", 5000);
+            employees[1] = new Employee("Maija", "002", "myyntipäällikkö", 3500);
+            employees[2] = new Employee("Pekka", "003", "suunnittelija", 3000);
+
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee.ToString());
+            }
+
+            Console.WriteLine(employees[0].CompareSalary(employees[1]));
+            Console.WriteLine(employees[0].CompareSalary(employees[2]));
+            Console.WriteLine(employees[2].CompareSalary(employees[1]));
+
+            Console.Write("Press any key to continue...\n");
+            Console.ReadKey();
+        }
+    }
+}
