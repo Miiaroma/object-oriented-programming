275ab6549198ac8f07d46f636b091f6403f4e492
Miiaroma <miia.romanainen@gmail.com>
2019-01-22 13:48:25 +0200

    Ticket price done.

---
+++ OOHarjoitukset/Ticket price/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ticket_price
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;            
+
+            Console.WriteLine("Ohjelma laskee maatalousnäyttelyn lipun hinnan\n");
+            Console.Write(" 1 = Aikuinen, 2 = Opiskelija, 3 = MTK:n jäsen, 4 - Opiskelija ja MTK:n jäsen," +
+            " 5 = Varusmies, Eläkeläinen (65 v. tai yli) tai Lapsi (7 - 15 v.), 6 = Lapsi (alle 7 v.) ");
+            
+            Ticket ticket = new Ticket();
+            ticket.AskData();
+            ticket.CountPrice();
+
+            Console.Write("Press any key to continue...\n");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOHarjoitukset/Ticket price/Ticket.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticket_price
+{
+    class Ticket
+    {
+        public int ticketPrice;
+        public double discount;
+        public int ticket;
+
+        public Ticket()
+        {
+            this.ticketPrice = 16;
+            this.discount = 0;           
+        }
+
+        public Ticket(int ticketPrice, double discount)
+        {
+            this.ticketPrice = ticketPrice;
+            this.discount = discount;
+        }
+
+        public void AskData()
+        {
+            Console.WriteLine("Anna lipputyypin numero: ");
+            string ticketValue = Console.ReadLine();
+
+            int.TryParse(ticketValue, out ticket);
+        }
+
+        public void CountPrice()
+        {
+            if (ticket == 1)
+                Console.WriteLine($"Lipun hintasi on {this.ticketPrice - this.ticketPrice * discount} €");
+            else if (ticket == 6)
+                Console.WriteLine($"Lipun hintasi on {this.ticketPrice - this.ticketPrice * 1.00} €");
+            else if (ticket == 2)
+                Console.WriteLine($"Lipun hintasi on {this.ticketPrice - this.ticketPrice * 0.15} €");
+            else if (ticket == 3)
+                Console.WriteLine($"Lipun hintasi on {this.ticketPrice - this.ticketPrice * 0.45} €");
+            else if (ticket == 4)
+                Console.WriteLine($"Lipun hintasi on {this.ticketPrice - this.ticketPrice * 0.60} €");
+            else
+                Console.WriteLine($"Lipun hintasi on {this.ticketPrice - this.ticketPrice * 0.50} €");
+        }
+    }
+}
+
