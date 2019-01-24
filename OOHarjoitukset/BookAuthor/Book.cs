ff509e5077ad7c3a615e3a7013034467174a35e4
Miiaroma <miia.romanainen@gmail.com>
2019-01-24 13:05:15 +0200

    BookAuthor program started, Book class done.

---
+++ OOHarjoitukset/BookAuthor/Book.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookAuthor
+{
+    class Book
+    {
+        public string title;
+        readonly string author;
+        public string publisher;
+        private double price;
+        public static string theme;
+
+        public Book()
+        {
+            this.title = String.Empty;
+            this.author = String.Empty;
+            this.publisher = String.Empty;
+            this.price = 0;
+            theme = String.Empty;
+        }
+
+        public Book(string title, string author, string publisher, double price, string themeName)
+        {
+            this.title = title;
+            this.author = author;
+            this.publisher = publisher;
+            this.price = price;
+            theme = themeName;
+        }
+
+        public void PrintBookInfo()
+        {
+            Console.WriteLine($"Kirjan tiedot ovat nimi: {this.title}\nKirjailija: {this.author}\nKustantaja: {this.publisher}\nHinta: {this.price:c}\nTeema: {theme}");
+        }
+
+        public void SearchBook()
+        {
+            Console.WriteLine("Etsi kirja, jonka nimi on: ");
+            string name = Console.ReadLine();
+
+            if (name == this.title)
+            {
+                Console.WriteLine($"Kirjan tiedot ovat nimi: {this.title}\nKirjailija: {this.author}\nKustantaja: {this.publisher}\nHinta: {this.price:c}\nTeema: {theme}");
+            }
+
+            else
+            {
+                Console.WriteLine("Kirjaa ei löytynyt.");
+            }
+        }
+        
+        public static void ChangeTheme()
+        {
+            Console.WriteLine("Anna uusi teema: ");
+            string themeName = Console.ReadLine();            
+        }
+
+        public double Price
+        {
+            get
+            {
+                return this.price;
+            }
+
+            set
+            {
+                if (value > 30)
+                {
+                    price = value * 0.9;
+                }
+
+                else
+                {
+                    price = value;
+                }
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return this.author;
+            }
+        }
+    }
+}
---
+++ OOHarjoitukset/BookAuthor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BookAuthor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirja- ja Kirjailija -luokka");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Book firstBook = new Book("Kirja1", "Kirjailija_A", "Otava", 0.00, "Tietokirja.");            
+            firstBook.Price = 30.5;
+            Book secondBook = new Book("Kirja2", "Kirjailija_B", "WSOY", 0.00, "Kaunokirjallisuus.");
+            secondBook.Price = 20.10;
+            Book thirdBook = new Book("Kirja3", "Kirjailija_C", "Otava", 0.00, "Lastenkirja.");
+            secondBook.Price = 20.10;
+
+            //Book.SearchBook();
+            Book.ChangeTheme();
+
+            Console.Write("Press any key to continue...\n");
+            Console.ReadKey();
+        }        
+    }
+}
