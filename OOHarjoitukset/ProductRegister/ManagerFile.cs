24307e97e3985d9e368e506341001cf11946eb6d
Miiaroma <miia.romanainen@gmail.com>
2019-03-07 20:27:13 +0200

    First version done.

---
+++ OOHarjoitukset/ProductRegister/ManagerFile.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace ProductRegister
+{
+    class ManagerFile
+    {
+        private string filePath;
+
+        public ManagerFile(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public string ListItems()
+        {
+            string content = String.Empty;
+
+            try
+            {
+                content = ReadFile();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine($"Virhe: {e.Message}");
+            }
+            return content;
+        }
+
+        private string ReadFile()
+        {
+            if (File.Exists(filePath))
+            {
+                List<Item> itemList = JsonConvert.DeserializeObject<List<Item>>(File.ReadAllText(this.filePath));
+
+                foreach (Item item in itemList)
+                {
+                    Console.WriteLine($"Nimi: {item.Name}\nTuotenumero: {item.Id}\nTuoteryhmä: {item.GroupName}");
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Haluatko tulostaa kaikki tuotetiedot? Vastaa K tai E.");
+                string userInput = Console.ReadLine().ToUpper();
+                if (userInput == "K")
+                {
+                    Console.WriteLine("Syötä tuotenumero: ");
+                    string id = Console.ReadLine();
+                    int.Parse(id);
+
+                    foreach (Item item in itemList)
+                    {
+                        if (id == item.Id)
+                        {
+                            Console.WriteLine($"Nimi: {item.Name}\nTuotenumero: {item.Id}\nTuoteryhmä: {item.GroupName}\nHinta: {item.Price}\nMäärä: {item.Amount}\nKommenttni: {item.Comment}\n");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Tietoja ei tulostettu");
+                }
+            }
+
+            else if (!File.Exists(filePath))
+            {
+                throw new Exception("Tiedostoa ei löydy");
+            }
+            return String.Empty;
+        }
+
+        public string ListProductGroup(string productGroup)
+        {
+            List<Item> itemList = JsonConvert.DeserializeObject<List<Item>>(File.ReadAllText(this.filePath));
+
+            foreach (Item item in itemList)
+            {
+                switch (productGroup)
+                {
+                    case "1":
+                    case "Lajittelu ja säilytys":
+                        break;
+
+                    case "2":
+                    case "Paperit ja lehtiöt":
+                        break;
+
+                    case "3":
+                    case "Kynät":
+                        break;
+
+                    case "4":
+                    case "Kortit ja kirjekuoret":
+                        break;
+
+                    default:
+                        Console.WriteLine("Väärä syöte. Valitse numeroista 1-4.");
+                        Console.ReadLine();
+                        break;
+                }
+                return $"Nimi: {item.Name}\nTuotenumero: {item.Id}\nTuoteryhmä: {item.GroupName}\nHinta: {item.Price}\nMäärä: {item.Amount}\nKommentti: {item.Comment}\n";
+            }
+            return "";
+        }
+
+        public void ListOutOfStock()
+        {
+            List<Item> itemList = JsonConvert.DeserializeObject<List<Item>>(File.ReadAllText(this.filePath));
+
+            foreach (Item item in itemList)
+            {
+                if (item.Amount == 0)
+                {
+                    Console.WriteLine($"Nimi: {item.Name}\nTuotenumero: {item.Id}\nTuoteryhmä: {item.GroupName}\nHinta: {item.Price}\nMäärä: {item.Amount}\nKommentti: {item.Comment}\n");
+                }
+            }            
+        }
+
+        public void OrderMore()
+        {
+            List<Item> itemList = JsonConvert.DeserializeObject<List<Item>>(File.ReadAllText(this.filePath));
+
+            Console.WriteLine("Syötä tuotenumero: ");
+            string id = Console.ReadLine();
+            int.Parse(id);
+
+            foreach (Item item in itemList)
+            {
+                if (id == item.Id && item.Amount == 0)
+                {
+                    Console.Write($"Lisää kommentti tuotteiden tilaamiseksi:");
+                    string comment = Console.ReadLine();
+                    item.Comment = comment;
+                    File.WriteAllText(filePath, JsonConvert.SerializeObject(itemList));
+
+                    Console.WriteLine($"Kommentti: {item.Comment}");
+                }
+            }  
+            Console.WriteLine("Tuotteita löytyy vielä varastosta");
+        }
+    }
+}
+
---
+++ OOHarjoitukset/ProductRegister/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ProductRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+            string message = string.Empty;
+            string filePath = @"C:\Dev\object-oriented-programming\OOHarjoitukset\ProductRegister\items.json";
+
+            ManagerFile managerFile = new ManagerFile(filePath);
+            Item item = new Item();
+
+            do
+            {
+                cki = UserInterface();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.F1:
+                        Console.WriteLine(managerFile.ListItems());
+                        message = "Valitut tuotetiedot listattu. \nPaina Enter jatkaaksesi!";
+                        break;
+
+                    case ConsoleKey.F2:
+                        Console.WriteLine("Valitse tuoteryhmä, syötä numero:\n" +
+                            "1 = Lajittelu ja säilytys\n" +
+                            "2 = Paperit ja lehtiöt\n" +
+                            "3 = Kynät\n" +
+                            "4 = Kortit ja kirjekuoret");
+                        string productGroup = Console.ReadLine();                        
+                        Console.WriteLine(managerFile.ListProductGroup(productGroup));
+                        message = "Valitun tuoteryhmän tiedot listattu.\nPaina Enter jatkaaksesi!";
+                        break;
+
+                    case ConsoleKey.F3: 
+                        managerFile.ListOutOfStock();
+                        managerFile.OrderMore();
+                        message = "Varastosta loppuneet tuotteet listattu ja tarvittavat toimenpiteet tehty. \nPaina Enter jatkaaksesi!"; 
+                        break;
+
+                    case ConsoleKey.Escape:
+                        message = "\nOhjelman suoritus päättyy.";
+                        break;
+                    default:
+                        message = "Virhe - Paina Enter ja aloita alusta!";
+                        break;
+                }
+                Console.WriteLine(message);
+                Console.ReadLine();
+                Console.Clear();
+            } while (cki.Key != ConsoleKey.Escape);
+
+            Console.WriteLine("Ohjelman suoritus päättyi!");
+        }
+
+        static ConsoleKeyInfo UserInterface()
+        {
+            Console.WriteLine("Tuoterekisteri");
+            Console.WriteLine("[F1] Listaa kaikki tuotteet");
+            Console.WriteLine("[F2] Listaa tietyn tuoteryhmän tiedot");
+            Console.WriteLine("[F3] Listaa varastosta loppuneet tuotteet");
+            Console.WriteLine("[ESC] Lopeta ohjelman suoritus");
+            Console.WriteLine();
+            Console.WriteLine("Valitse vaihtoehto:");
+
+            return Console.ReadKey();
+        }
+
+    }
+}
